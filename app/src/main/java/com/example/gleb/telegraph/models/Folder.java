18966aff53bb26d02f833b7563fe544f04095af1
GlebKalinichenko <Glebjn@yandex.ua>
2015-12-30 10:34:54 +0400

    Description folder from database in app

---
+++ app/src/main/java/com/example/gleb/telegraph/models/Folder.java
@@ -0,0 +1,20 @@
+package com.example.gleb.telegraph.models;
+
+/**
+ * Created by Gleb on 30.12.2015.
+ */
+public class Folder {
+    private String folder;
+
+    public Folder(String folder) {
+        this.folder = folder;
+    }
+
+    public String getFolder() {
+        return folder;
+    }
+
+    public void setFolder(String folder) {
+        this.folder = folder;
+    }
+}
