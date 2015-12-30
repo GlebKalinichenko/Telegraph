e1db372bea7136630a200d84216f7e884547196c
GlebKalinichenko <Glebjn@yandex.ua>
2015-12-30 10:32:20 +0400

    Abstract for get saved keys and signatire from root folder

---
+++ app/src/main/java/com/example/gleb/telegraph/abstracts/AbstractDatabaseCryptography.java
@@ -0,0 +1,20 @@
+package com.example.gleb.telegraph.abstracts;
+
+/**
+ * Created by Gleb on 30.12.2015.
+ */
+public class AbstractDatabaseCryptography {
+    protected String linkToFolder;
+
+    public AbstractDatabaseCryptography(String linkToFolder) {
+        this.linkToFolder = linkToFolder;
+    }
+
+    public String getLinkToFolder() {
+        return linkToFolder;
+    }
+
+    public void setLinkToFolder(String linkToFolder) {
+        this.linkToFolder = linkToFolder;
+    }
+}
