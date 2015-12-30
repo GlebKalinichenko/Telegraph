be344ef833e74e5a84adc3d880e07523ca847455
GlebKalinichenko <Glebjn@yandex.ua>
2015-12-30 10:33:21 +0400

    Abstract for description all users usedin app

---
+++ app/src/main/java/com/example/gleb/telegraph/abstracts/AbstractUser.java
@@ -0,0 +1,20 @@
+package com.example.gleb.telegraph.abstracts;
+
+/**
+ * Created by Gleb on 30.12.2015.
+ */
+public class AbstractUser {
+    protected String email;
+
+    public AbstractUser(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
