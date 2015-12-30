aa042f6478ab2f9cdffc4f25637c5e6705a1a6fa
GlebKalinichenko <Glebjn@yandex.ua>
2015-12-30 10:43:16 +0400

    Description user that send encryption symetrical key

---
+++ app/src/main/java/com/example/gleb/telegraph/models/User.java
@@ -0,0 +1,12 @@
+package com.example.gleb.telegraph.models;
+
+import com.example.gleb.telegraph.abstracts.AbstractUser;
+
+/**
+ * Created by Gleb on 30.12.2015.
+ */
+public class User extends AbstractUser {
+    public User(String email) {
+        super(email);
+    }
+}
