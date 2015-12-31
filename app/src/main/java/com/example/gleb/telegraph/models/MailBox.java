d289f75297fecfd2011e87df26644f1d2ab75fd7
GlebKalinichenko <Glebjn@yandex.ua>
2015-12-31 17:08:48 +0400

    Parse email for get name of post server

--- app/src/main/java/com/example/gleb/telegraph/models/MailBox.java
+++ app/src/main/java/com/example/gleb/telegraph/models/MailBox.java
@@ -12,6 +12,18 @@ public class MailBox {
         this.password = password;
     }
 
+    /**
+     * Parse email for get name of post server
+     * @param String        Email of post account
+     * @return String       Name of post server
+     * */
+    public static String parseEmail(String email){
+        int beginChar = email.indexOf("@") + 1;
+        int endChar = email.indexOf(".");
+        email = email.substring(beginChar, endChar);
+        return email;
+    }
+
     public String getEmail() {
         return email;
     }
