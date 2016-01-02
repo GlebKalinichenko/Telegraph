c2762fc9bbd9a4388109e2f6bda0e1c9dbaaa5ae
GlebKalinichenko <Glebjn@yandex.ua>
2016-01-02 11:36:17 +0400

    Parse xml with settings of email account and instance settings

--- app/src/main/java/com/example/gleb/telegraph/models/MailSettings.java
+++ app/src/main/java/com/example/gleb/telegraph/models/MailSettings.java
@@ -2,8 +2,12 @@ package com.example.gleb.telegraph.models;
 
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
 
 import com.example.gleb.telegraph.DatabaseHelper;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 
 /**
  * Created by Gleb on 30.12.2015.
@@ -17,14 +21,29 @@ public class MailSettings {
     private String addressSmtp;
     private String portSmtp;
 
-    public MailSettings(String namePostServer, String addressImap, String portImap, String addressPop3, String portPop3, String addressSmtp, String portSmtp) {
-        this.namePostServer = namePostServer;
-        this.addressImap = addressImap;
-        this.portImap = portImap;
-        this.addressPop3 = addressPop3;
-        this.portPop3 = portPop3;
-        this.addressSmtp = addressSmtp;
-        this.portSmtp = portSmtp;
+    public static MailSettings newInstance(String urlServer, Document doc) {
+        doc.getDocumentElement().normalize();
+        NodeList hostList = doc.getElementsByTagName("hostname");
+        NodeList portList = doc.getElementsByTagName("port");
+
+        String imapHost = hostList.item(0).getFirstChild().getNodeValue();
+        String pop3Host = hostList.item(1).getFirstChild().getNodeValue();
+        String smtpHost = hostList.item(2).getFirstChild().getNodeValue();
+
+        String imapPort = portList.item(0).getFirstChild().getNodeValue();
+        String pop3Port = portList.item(1).getFirstChild().getNodeValue();
+        String smtpPort = portList.item(2).getFirstChild().getNodeValue();
+
+        MailSettings mailSettings = new MailSettings();
+        mailSettings.setNamePostServer(urlServer);
+        mailSettings.setAddressImap(imapHost);
+        mailSettings.setPortImap(imapPort);
+        mailSettings.setAddressPop3(pop3Host);
+        mailSettings.setPortPop3(pop3Port);
+        mailSettings.setAddressSmtp(smtpHost);
+        mailSettings.setPortSmtp(smtpPort);
+
+        return mailSettings;
     }
 
     public String getNamePostServer() {
