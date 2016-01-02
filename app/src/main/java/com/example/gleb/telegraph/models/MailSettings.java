e031b081e102d73063a55a4dc08127e389c26902
GlebKalinichenko <Glebjn@yandex.ua>
2016-01-02 12:23:22 +0400

    Write settings to database

--- app/src/main/java/com/example/gleb/telegraph/models/MailSettings.java
+++ app/src/main/java/com/example/gleb/telegraph/models/MailSettings.java
@@ -1,5 +1,6 @@
 package com.example.gleb.telegraph.models;
 
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -21,6 +22,12 @@ public class MailSettings {
     private String addressSmtp;
     private String portSmtp;
 
+    /**
+     * Parse settings from api in format xml and create settings
+     * @param String            Url address of server
+     * @param Document          Settings gets from server in format xml
+     * @return MailSettings     Created settings
+     * */
     public static MailSettings newInstance(String urlServer, Document doc) {
         doc.getDocumentElement().normalize();
         NodeList hostList = doc.getElementsByTagName("hostname");
@@ -44,6 +51,25 @@ public class MailSettings {
         mailSettings.setPortSmtp(smtpPort);
 
         return mailSettings;
+    }
+
+    /**
+     * Add settings to database
+     * @param SQLiteDatabase        Database
+     * @return void
+     * */
+    public void addSettings(SQLiteDatabase sdb){
+        ContentValues values = new ContentValues();
+        values.put(DatabaseHelper.NAME_POST_SERVER, this.namePostServer);
+        values.put(DatabaseHelper.ADDRESS_IMAP, this.addressImap);
+        values.put(DatabaseHelper.PORT_IMAP, this.portImap);
+        values.put(DatabaseHelper.ADDRESS_POP3, this.addressPop3);
+        values.put(DatabaseHelper.PORT_POP3, this.portPop3);
+        values.put(DatabaseHelper.ADDRESS_SMTP, this.addressSmtp);
+        values.put(DatabaseHelper.PORT_SMTP, this.portSmtp);
+
+        sdb.insert(DatabaseHelper.TABLE_MAIL_SETTINGS, null, values);
+        sdb.close();
     }
 
     public String getNamePostServer() {
