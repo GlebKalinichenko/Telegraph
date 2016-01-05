7c1fc05778cce4c6dc6e9ebef8796cb59fb7cbee
GlebKalinichenko <Glebjn@yandex.ua>
2016-01-05 16:24:56 +0400

    Index of send and receive mail

---
+++ app/src/main/java/com/example/gleb/telegraph/models/StraightIndex.java
@@ -0,0 +1,9 @@
+package com.example.gleb.telegraph.models;
+
+/**
+ * Created by Gleb on 05.01.2016.
+ */
+public class StraightIndex {
+    public static final int STRAIGHT = 1;
+    public static final int REVERSE = 0;
+}
