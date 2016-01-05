6b6b1ec31942f3168f4623d47fdcc36e1318166c
GlebKalinichenko <Glebjn@yandex.ua>
2016-01-05 19:42:57 +0400

    Select all mails from database

--- app/src/main/java/com/example/gleb/telegraph/models/Mail.java
+++ app/src/main/java/com/example/gleb/telegraph/models/Mail.java
@@ -1,9 +1,13 @@
 package com.example.gleb.telegraph.models;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.example.gleb.telegraph.DatabaseHelper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Gleb on 30.12.2015.
@@ -15,9 +19,10 @@ public class Mail {
     private String subject;
     private String content;
     private String date;
-    private boolean hasAttach;
+    private int hasAttach;
+    private int straightIndex;
 
-    public Mail(String sender, String nameSender, String receiver, String subject, String content, String date, boolean hasAttach) {
+    public Mail(String sender, String nameSender, String receiver, String subject, String content, String date, int hasAttach, int straightIndex) {
         this.sender = sender;
         this.nameSender = nameSender;
         this.receiver = receiver;
@@ -25,6 +30,7 @@ public class Mail {
         this.content = content;
         this.date = date;
         this.hasAttach = hasAttach;
+        this.straightIndex = straightIndex;
     }
 
     /**
@@ -35,7 +41,7 @@ public class Mail {
      * @param int                   Type mail 0 is sent mail 1 is received mail
      * @return void
      * */
-    public void addMail(SQLiteDatabase sdb, int userCode, int folderCode, int straightIndex){
+    public void addMail(SQLiteDatabase sdb, int userCode, int folderCode){
         ContentValues values = new ContentValues();
         values.put(DatabaseHelper.SENDER_USER_CODE, userCode);
         values.put(DatabaseHelper.NAME_SENDER, this.nameSender);
@@ -45,8 +51,47 @@ public class Mail {
         values.put(DatabaseHelper.DATE, this.date);
         values.put(DatabaseHelper.FOLDER_CODE, folderCode);
         values.put(DatabaseHelper.HAS_ATTACH, this.hasAttach);
-        values.put(DatabaseHelper.STRAIGHT_INDEX, straightIndex);
+        values.put(DatabaseHelper.STRAIGHT_INDEX, this.straightIndex);
         sdb.insert(DatabaseHelper.TABLE_MAILS, null, values);
+    }
+
+    /**
+     * Select all mails
+     * @param SQLiteDatabase        Database
+     * @return List<Mail>           List of mails
+     * */
+    public static List<Mail> selectAllMail(SQLiteDatabase sdb){
+        List<Mail> mails = new ArrayList<>();
+        Cursor cursor = sdb.query(DatabaseHelper.TABLE_MAILS,
+                new String[]{DatabaseHelper.ID_MAIL, DatabaseHelper.SENDER_USER_CODE,
+                DatabaseHelper.NAME_SENDER, DatabaseHelper.RECEIVER, DatabaseHelper.SUBJECT,
+                DatabaseHelper.CONTENT, DatabaseHelper.DATE, DatabaseHelper.FOLDER_CODE,
+                DatabaseHelper.HAS_ATTACH, DatabaseHelper.STRAIGHT_INDEX},
+                null, null, null, null, null);
+        if (cursor != null && cursor.moveToFirst()) {
+            do {
+                int id = cursor.getInt(0);
+                int senderCode = cursor.getInt(1);
+                Cursor cursorUser = sdb.query(DatabaseHelper.TABLE_USERS, new String[]{
+                        DatabaseHelper.EMAIL_USER}, DatabaseHelper.ID_USER + "=?",
+                        new String[]{String.valueOf(senderCode)}, null, null, null, null);
+                if (cursorUser != null && cursorUser.moveToFirst()) {
+                    String sender = cursorUser.getString(0);
+                    String namesender = cursor.getString(2);
+                    String receiver = cursor.getString(3);
+                    String subject = cursor.getString(4);
+                    String content = cursor.getString(5);
+                    String date = cursor.getString(6);
+                    int hasAttach = cursor.getInt(7);
+                    int straightIndex = cursor.getInt(8);
+                    Mail mail = new Mail(sender, namesender, receiver, subject, content, date,
+                            hasAttach, straightIndex);
+                    mails.add(mail);
+                }
+            }
+            while (cursor.moveToNext());
+        }
+        return mails;
     }
 
     public String getNameSender() {
@@ -97,11 +142,11 @@ public class Mail {
         this.date = date;
     }
 
-    public boolean isHasAttach() {
+    public int isHasAttach() {
         return hasAttach;
     }
 
-    public void setHasAttach(boolean hasAttach) {
+    public void setHasAttach(int hasAttach) {
         this.hasAttach = hasAttach;
     }
 }
