3e83cf3cea0be523c52971e5388c65d87ce12dd5
GlebKalinichenko <Glebjn@yandex.ua>
2016-01-06 14:55:07 +0400

    Select all mails with use raw query

--- app/src/main/java/com/example/gleb/telegraph/models/Attach.java
+++ app/src/main/java/com/example/gleb/telegraph/models/Attach.java
@@ -1,30 +1,43 @@
 package com.example.gleb.telegraph.models;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.gleb.telegraph.DatabaseHelper;
+
 /**
  * Created by Gleb on 30.12.2015.
  */
 public class Attach {
-    private String[] nameAttachs;
-    private int[] numPositions;
+    private String nameAttachs;
+    private int numPositions;
 
-    public Attach(String[] nameAttachs, int[] numPositions) {
+    public Attach(String nameAttachs, int numPositions) {
         this.nameAttachs = nameAttachs;
         this.numPositions = numPositions;
     }
 
-    public String[] getNameAttachs() {
+    public void addAttach(SQLiteDatabase sdb, int mailCode){
+        ContentValues values = new ContentValues();
+        values.put(DatabaseHelper.NAME_ATTACH, this.nameAttachs);
+        values.put(DatabaseHelper.NUM_POSITION, this.numPositions);
+        values.put(DatabaseHelper.MAIL_CODE, mailCode);
+        sdb.insert(DatabaseHelper.TABLE_ATTACHS, null, values);
+    }
+
+    public String getNameAttachs() {
         return nameAttachs;
     }
 
-    public void setNameAttachs(String[] nameAttachs) {
+    public void setNameAttachs(String nameAttachs) {
         this.nameAttachs = nameAttachs;
     }
 
-    public int[] getNumPositions() {
+    public int getNumPositions() {
         return numPositions;
     }
 
-    public void setNumPositions(int[] numPositions) {
+    public void setNumPositions(int numPositions) {
         this.numPositions = numPositions;
     }
 }
