f36b8b8281fb02267d2059d446e6da851a35fdcf
GlebKalinichenko <Glebjn@yandex.ua>
2016-01-08 16:35:08 +0400

    Add array of folders for database

--- app/src/main/java/com/example/gleb/telegraph/models/MailFolder.java
+++ app/src/main/java/com/example/gleb/telegraph/models/MailFolder.java
@@ -3,8 +3,11 @@ package com.example.gleb.telegraph.models;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
 
 import com.example.gleb.telegraph.DatabaseHelper;
+
+import javax.mail.Folder;
 
 /**
  * Created by Gleb on 30.12.2015.
@@ -35,6 +38,34 @@ public class MailFolder {
         values.put(DatabaseHelper.NAME_FOLDER, this.folder);
         values.put(DatabaseHelper.MAIL_BOX_CODE, boxCode);
         sdb.insert(DatabaseHelper.TABLE_FOLDERS, null, values);
+    }
+
+    /**
+     * Add array of folders to database
+     * @param DatabaseHelper        Helper for work with database
+     * @param Folder[]              Array of folders
+     * @return void
+     * */
+    public static void addFolders(DatabaseHelper databaseHelper, Folder[] folders){
+        SQLiteDatabase sdb = databaseHelper.getWritableDatabase();
+        SQLiteDatabase db = databaseHelper.getReadableDatabase();
+
+        String sql = "insert into Folders (NameFolder, MailBoxCode) values (?, ?);";
+        SQLiteStatement stmt = sdb.compileStatement(sql);
+        sdb.beginTransaction();
+        for (Folder f : folders){
+            MailFolder folder = new MailFolder(f.getName());
+//            folder.addFolder(sdb, MailBox.getLastAccount(db));
+            stmt.bindString(1, folder.getFolder());
+            //get code of folder from database
+            stmt.bindLong(2, MailBox.getLastAccount(db));
+            long entryID = stmt.executeInsert();
+            stmt.clearBindings();
+        }
+        sdb.setTransactionSuccessful();
+        sdb.endTransaction();
+        sdb.close();
+        db.close();
     }
 
     /**
