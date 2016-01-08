ba98fa62da0a5fb6595df55abaa1d2bf5268f1fe
GlebKalinichenko <Glebjn@yandex.ua>
2016-01-08 09:41:52 +0400

    Select query for get all mails from database

--- app/src/main/java/com/example/gleb/telegraph/models/Attach.java
+++ app/src/main/java/com/example/gleb/telegraph/models/Attach.java
@@ -1,9 +1,13 @@
 package com.example.gleb.telegraph.models;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.example.gleb.telegraph.DatabaseHelper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Gleb on 30.12.2015.
@@ -29,6 +33,27 @@ public class Attach {
         values.put(DatabaseHelper.NUM_POSITION, this.numPositions);
         values.put(DatabaseHelper.MAIL_CODE, mailCode);
         sdb.insert(DatabaseHelper.TABLE_ATTACHS, null, values);
+    }
+
+    /**
+     * Select all attach
+     * @param SQLiteDatabase        Database
+     * @return List<Attach>         List of attachs
+     * */
+    public static List<Attach> selectAllAttach(SQLiteDatabase sdb){
+        List<Attach> attachs = new ArrayList<>();
+        String query = "Select Attachs.NameAttach, Attachs.NumPositionOfAttach from Attachs";
+        Cursor cursor = sdb.rawQuery(query, null);
+        if (cursor != null && cursor.moveToFirst()) {
+            do {
+                String nameAttach = cursor.getString(0);
+                int numPosition = cursor.getInt(1);
+                Attach attach = new Attach(nameAttach, numPosition);
+                attachs.add(attach);
+            }
+            while (cursor.moveToNext());
+        }
+        return attachs;
     }
 
     public String getNameAttachs() {
