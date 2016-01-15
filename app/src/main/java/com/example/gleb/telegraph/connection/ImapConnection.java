783a5019d97f9ec0c41ccb5de043ec1194a64c61
GlebKalinichenko <Glebjn@yandex.ua>
2016-01-15 08:51:15 +0300

    Connection to post server by using imap protochol

---
+++ app/src/main/java/com/example/gleb/telegraph/connection/ImapConnection.java
@@ -0,0 +1,50 @@
+package com.example.gleb.telegraph.connection;
+
+import com.example.gleb.telegraph.abstracts.AbstractConnection;
+import com.example.gleb.telegraph.models.MailBox;
+import com.example.gleb.telegraph.models.MailSettings;
+
+import java.util.Properties;
+
+import javax.mail.MessagingException;
+import javax.mail.NoSuchProviderException;
+import javax.mail.Session;
+import javax.mail.Store;
+
+/**
+ * Created by gleb on 14.01.16.
+ */
+public class ImapConnection extends AbstractConnection {
+
+    /**
+     * Authentication on post server
+     * @param String        Email of account
+     * @param String        Password of account
+     * @return boolean      Is authentication complete
+     * */
+    @Override
+    public Store authentication(MailSettings mailSettings, MailBox mailBox) {
+        Properties props = new Properties();
+        props.put("mail.imap.port", mailSettings.getPortImap());
+        props.put("mail.imap.socketFactory.port", mailSettings.getPortImap());
+        props.put("mail.imap.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.imap.socketFactory.fallback", "false");
+        props.setProperty("mail.store.protocol", "imaps");
+
+        Session session = Session.getInstance(props, null);
+        Store store = null;
+        try {
+            store = session.getStore();
+        } catch (NoSuchProviderException e) {
+            e.printStackTrace();
+            return null;
+        }
+        try {
+            store.connect(mailSettings.getAddressImap(), mailBox.getEmail(), mailBox.getPassword());
+            return store;
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
