619c1891e18e5d29bb3babf4d44ba2ab4e641476
GlebKalinichenko <Glebjn@yandex.ua>
2016-01-18 20:48:19 +0300

    Action toogle button for navigation drawer

--- app/src/main/java/com/example/gleb/telegraph/abstracts/AbstractActivity.java
+++ app/src/main/java/com/example/gleb/telegraph/abstracts/AbstractActivity.java
@@ -1,10 +1,13 @@
 package com.example.gleb.telegraph.abstracts;
 
+import android.content.res.TypedArray;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
 
 import com.example.gleb.telegraph.DatabaseHelper;
+import com.example.gleb.telegraph.navigationdrawer.NavDrawerAdapter;
 
 /**
  * Created by Gleb on 03.01.2016.
@@ -13,5 +16,9 @@ public abstract class AbstractActivity extends AppCompatActivity {
     protected DatabaseHelper databaseHelper;
     protected ListView navDrawerListView;
     protected DrawerLayout drawerLayout;
+    protected ActionBarDrawerToggle drawerToggle;
+    protected String[] navMenuTitles;
+    protected TypedArray navMenuIcons;
+    protected NavDrawerAdapter navDrawerAdapter;
     protected abstract void initializeWidgets();
 }
