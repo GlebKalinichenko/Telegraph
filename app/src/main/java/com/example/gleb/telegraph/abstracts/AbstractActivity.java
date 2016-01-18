74596de7fefcedb0880b3a74e4a8399ebd24e488
GlebKalinichenko <Glebjn@yandex.ua>
2016-01-18 14:13:55 +0300

    List of navigation drawer and drawer layout for call navigation drawer

--- app/src/main/java/com/example/gleb/telegraph/abstracts/AbstractActivity.java
+++ app/src/main/java/com/example/gleb/telegraph/abstracts/AbstractActivity.java
@@ -1,6 +1,8 @@
 package com.example.gleb.telegraph.abstracts;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
 import com.example.gleb.telegraph.DatabaseHelper;
 
@@ -9,5 +11,7 @@ import com.example.gleb.telegraph.DatabaseHelper;
  */
 public abstract class AbstractActivity extends AppCompatActivity {
     protected DatabaseHelper databaseHelper;
+    protected ListView navDrawerListView;
+    protected DrawerLayout drawerLayout;
     protected abstract void initializeWidgets();
 }
