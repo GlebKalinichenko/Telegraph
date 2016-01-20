92bc2bfe54f632e23c8da08602800d063ea7c9b8
GlebKalinichenko <Glebjn@yandex.ua>
2016-01-20 18:39:12 +0300

    Abstract parent for all fragments in app

---
+++ app/src/main/java/com/example/gleb/telegraph/abstracts/AbstractFragment.java
@@ -0,0 +1,12 @@
+package com.example.gleb.telegraph.abstracts;
+
+import android.support.v4.app.Fragment;
+
+import com.example.gleb.telegraph.DatabaseHelper;
+
+/**
+ * Created by gleb on 20.01.16.
+ */
+public class AbstractFragment extends Fragment {
+    protected DatabaseHelper databaseHelper;
+}
