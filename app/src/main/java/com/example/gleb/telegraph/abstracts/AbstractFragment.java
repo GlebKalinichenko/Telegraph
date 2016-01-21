01869043b51a3c14beefc02b173a3beb9035428a
GlebKalinichenko <Glebjn@yandex.ua>
2016-01-21 16:53:27 +0300

    Recycler view and adapter for show mails in base fragment

--- app/src/main/java/com/example/gleb/telegraph/abstracts/AbstractFragment.java
+++ app/src/main/java/com/example/gleb/telegraph/abstracts/AbstractFragment.java
@@ -1,12 +1,15 @@
 package com.example.gleb.telegraph.abstracts;
 
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 
 import com.example.gleb.telegraph.DatabaseHelper;
+import com.example.gleb.telegraph.MailAdapter;
 
 /**
  * Created by gleb on 20.01.16.
  */
 public class AbstractFragment extends Fragment {
-    protected DatabaseHelper databaseHelper;
+    protected RecyclerView recyclerView;
+    protected MailAdapter mailAdapter;
 }
