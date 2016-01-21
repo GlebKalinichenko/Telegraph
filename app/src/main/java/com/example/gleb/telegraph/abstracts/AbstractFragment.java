f516233661eb1e27b9b74106070e0ea78f082530
GlebKalinichenko <Glebjn@yandex.ua>
2016-01-21 18:07:46 +0300

    Base for activities

--- app/src/main/java/com/example/gleb/telegraph/abstracts/AbstractFragment.java
+++ app/src/main/java/com/example/gleb/telegraph/abstracts/AbstractFragment.java
@@ -2,14 +2,20 @@ package com.example.gleb.telegraph.abstracts;
 
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.example.gleb.telegraph.DatabaseHelper;
 import com.example.gleb.telegraph.MailAdapter;
 
+import javax.mail.internet.AddressException;
+
 /**
  * Created by gleb on 20.01.16.
  */
-public class AbstractFragment extends Fragment {
+public abstract class AbstractFragment extends Fragment {
     protected RecyclerView recyclerView;
     protected MailAdapter mailAdapter;
+    protected abstract View initializeWidgets(LayoutInflater inflater, ViewGroup container);
 }
