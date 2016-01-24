d6933b91569238faf63a7d10a03250655e9b22ee
GlebKalinichenko <Glebjn@yandex.ua>
2016-01-24 16:16:59 +0300

    Description of use protocols

---
+++ app/src/main/java/com/example/gleb/telegraph/connection/Protocols.java
@@ -0,0 +1,10 @@
+package com.example.gleb.telegraph.connection;
+
+/**
+ * Created by gleb on 24.01.16.
+ */
+public class Protocols {
+    public static final String IMAP = "Imap";
+    public static final String POP3 = "Pop3";
+    public static final String SMTP = "Smtp";
+}
