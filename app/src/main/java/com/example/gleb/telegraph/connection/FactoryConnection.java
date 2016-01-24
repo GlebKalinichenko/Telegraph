ba47aad8deb4ea81614444a66356182cc52a2b17
GlebKalinichenko <Glebjn@yandex.ua>
2016-01-24 16:15:05 +0300

    Authentication of post server

--- app/src/main/java/com/example/gleb/telegraph/connection/FactoryConnection.java
+++ app/src/main/java/com/example/gleb/telegraph/connection/FactoryConnection.java
@@ -1,34 +1,62 @@
 package com.example.gleb.telegraph.connection;
 
 import com.example.gleb.telegraph.abstracts.AbstractConnection;
-import com.example.gleb.telegraph.connection.ImapConnection;
-import com.example.gleb.telegraph.connection.Pop3Connection;
 import com.example.gleb.telegraph.models.MailBox;
 import com.example.gleb.telegraph.models.MailSettings;
 
+import javax.mail.MessagingException;
+import javax.mail.NoSuchProviderException;
+import javax.mail.Session;
 import javax.mail.Store;
 
 /**
  * Created by gleb on 14.01.16.
  */
 public class FactoryConnection {
-    private Store store;
+    private Session session;
 
     /**
-     * Get store with messages from post server
+     * Get session with messages from post server
      * @param MailSettings        Settings of post server
-     * @param MailBox             Mail of box with email and password email account
+     * @param String              Use protocol
      * @return Store              Store of messages from post server
      * */
-    public Store getStore(MailSettings mailSettings, MailBox mailBox) {
+    public Session getSession(MailSettings mailSettings, String protocol) {
         AbstractConnection connection = null;
-        if (mailBox.getUseProtocol().equals("imap")) {
+        if (protocol.equals(Protocols.IMAP)) {
             connection = new ImapConnection();
-            store = connection.authentication(mailSettings, mailBox);
-        } else if (!mailBox.getUseProtocol().equals("imap")) {
+            session = connection.settingProperties(mailSettings);
+        } else if (protocol.equals(Protocols.POP3)) {
             connection = new Pop3Connection();
-            store = connection.authentication(mailSettings, mailBox);
+            session = connection.settingProperties(mailSettings);
+        } else if (protocol.equals(Protocols.SMTP)){
+            connection = new SmtpConnection();
+            session = connection.settingProperties(mailSettings);
         }
-        return store;
+        return session;
+    }
+
+    /**
+     * Authentication on post server
+     * @param Session             Settings of post server
+     * @param MailSettings        Settings of mail
+     * @return Store              Store of messages from post server
+     * */
+    public Store authentication(Session session, MailSettings mailSettings,
+        MailBox mailBox) {
+        Store store = null;
+        try {
+            store = session.getStore();
+        } catch (NoSuchProviderException e) {
+            e.printStackTrace();
+            return null;
+        }
+        try {
+            store.connect(mailSettings.getAddressImap(), mailBox.getEmail(), mailBox.getPassword());
+            return store;
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
