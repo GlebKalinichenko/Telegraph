0a77ae42aa54691aef2b34888deea4c921e1cbe8
GlebKalinichenko <Glebjn@yandex.ua>
2016-01-25 10:30:49 +0300

    Send subject for mail

--- app/src/main/java/com/example/gleb/telegraph/SendMailInterface.java
+++ app/src/main/java/com/example/gleb/telegraph/SendMailInterface.java
@@ -1,9 +1,12 @@
 package com.example.gleb.telegraph;
 
+import com.example.gleb.telegraph.models.MailBox;
+import com.example.gleb.telegraph.models.MailSettings;
+
 /**
  * Created by gleb on 24.01.16.
  */
 public interface SendMailInterface {
-    public boolean sendMail(String message, String[] receivers, boolean hasEncryption,
-        boolean hasDigest);
+    public boolean sendMail(String subject, String message, String[] receivers, boolean hasEncryption,
+        boolean hasDigest, MailSettings mailSettings, MailBox mailBox);
 }
