3a58b2cc8f6587df353581e570833992e7020510
GlebKalinichenko <Glebjn@yandex.ua>
2016-02-01 19:29:33 +0300

    Service for gets new mails

--- app/src/main/java/com/example/gleb/telegraph/MailService.java
+++ app/src/main/java/com/example/gleb/telegraph/MailService.java
@@ -2,9 +2,16 @@ package com.example.gleb.telegraph;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MailService extends Service {
+    public static final long NOTIFY_INTERVAL = 10 * 1000;
+    private Handler mHandler = new Handler();
+    private Timer mTimer = null;
     public MailService() {
     }
 
@@ -12,5 +19,30 @@ public class MailService extends Service {
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if(mTimer != null) {
+            mTimer.cancel();
+        } else {
+            mTimer = new Timer();
+        }
+        mTimer.scheduleAtFixedRate(new TimeDisplayTimerTask(), 0, NOTIFY_INTERVAL);
+    }
+
+    class TimeDisplayTimerTask extends TimerTask {
+        @Override
+        public void run() {
+            // run on another thread
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    
+                }
+
+            });
+        }
     }
 }
