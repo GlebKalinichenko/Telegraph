d57c407f30fb26c6d41686b3bc8c7b6d30d119d9
GlebKalinichenko <Glebjn@yandex.ua>
2016-02-06 18:22:16 +0300

    Folders for sync

---
+++ app/src/main/java/com/example/gleb/telegraph/syncmails/SyncTypeMails.java
@@ -0,0 +1,9 @@
+package com.example.gleb.telegraph.syncmails;
+
+/**
+ * Created by gleb on 06.02.16.
+ */
+public class SyncTypeMails {
+    public static final String SYNC_SEND_MAIL = "Send";
+    public static final String SYNC_TRASH_MAIL = "Trash";
+}
